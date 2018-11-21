ee7991851c97663cc2bf6df18a0caf65f15b17f2
Dmytro <krasilnikov.dmytro@gmail.com>
2018-11-21 17:11:50 +0200

    kreate a buttons of counters and smiles

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
 import React, { Component } from 'react';
+import ReactDom from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+
+    this.fun = this.fun.bind(this);
+    this.sad = this.sad.bind(this);
+
+    this.state = {
+      count: this.props.initialValue,
+    };
+  }
+
+  increment() {
+    this.setState((state) => ({
+      count: state.count + 1,
+    }))
+  }
+
+  decrement() {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+
+  fun() {
+    this.setState((state) => ({
+      smile: state.smile = ":)",
+    }))
+  }
+
+  sad() {
+    this.setState({
+      smile: this.state.smile = ":(",
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    <>
+      <button onClick={this.increment}>+</button>
+      <button onClick={this.fun}>Fun!</button>
+      <div>
+          {this.state.count}
       </div>
+      <div>
+          {this.state.smile}
+      </div>
+      <button onClick={this.decrement}>-</button>
+      <button onClick={this.sad}>Sad...</button>
+    </>
     );
   }
 }
+const rootElement = document.getElementById('root');
+ReactDom.render(<App initialValue = {0} />, rootElement);
 
 export default App;
